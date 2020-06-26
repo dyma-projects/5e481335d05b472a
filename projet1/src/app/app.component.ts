8e2d9d2d304b0ad8179bb7eb18a6c45a7c54829e
Anshen Kondjah <plouisin@gmail.com>
2020-06-26 11:25:29 +0200

    projet1

---
+++ projet1/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'projet1';
+  bonjourLequipe = "Bonjour à l'équipe des formateurs"
+}
